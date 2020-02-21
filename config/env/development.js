63eac144083fa9bc5972c661f07cd9b55d3673e9
Pruthvi Patel <pruthvipatel145@gmail.com>
2020-02-21 19:00:19 +0530

    removed unncessary env vars

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,5 @@ var jsonfile = require("jsonfile");
 var envVars = jsonfile.readFileSync(envFile);
 
 module.exports = {
-  NODE_ENV: envVars["NODE_ENV"],
-  MONGODB_CONNECTION: envVars["MONGODB_CONNECTION"],
-  SENDGRID_API_KEY: envVars["SENDGRID_API_KEY"]
+  MONGODB_CONNECTION: envVars["MONGODB_CONNECTION"]
 };
